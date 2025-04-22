849c2690c53cdf9d3420df7f3a2f9e2e1a7f4abb
Charly <carlosecimino@gmail.com>
2025-04-22 15:14:28 -0300

    Métodos de ArrayList

--- src/resueltos/guia2/ej01/Test.java
+++ src/resueltos/guia2/ej01/Test.java
@@ -7,6 +7,22 @@ package resueltos.guia2.ej01;
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
+
+/*
+    Métodos más comunes de ArrayList
+
+    add(Elemento) agrega al final
+    add(index, Elemento) agrega en la posición index, corriendo lo demás un lugar a la derecha
+    remove(index): Remueve por posicion, retorna el objeto borrado
+    remove(Elemento): Remueve por referencia, retorna bollean si pudo o no
+    get(index): Devuelve el elemento en la posicion index.
+    set(index, Elemento): Reemplaza al elemento en la posición index.
+    size(): Retorna la cantidad de elementos de la lista
+    isEmpty(): Devuelve si está o no vacía.
+    contains(Elemento): Retorna si está o no en la lista
+*/
+
+
 public class Test {
     public static void main(String[] args) {
         Agenda agenda = new Agenda();
