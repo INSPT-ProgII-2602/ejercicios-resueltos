fd6b40e4dd2c54a6ed344e94f923ff5098655f2b
Charly <carlosecimino@gmail.com>
2025-05-27 14:44:22 -0300

    Guia3-Ej03 recien exportado a Java desde UMLetino

---
+++ src/resueltos/guia3/ej03/Carta.java
@@ -0,0 +1,23 @@
+package resueltos.guia3.ej03;
+
+/**
+* Código generado por la app UXFtoJava by Charly Cimino
+* @see https://github.com/CharlyCimino/uxf-to-java
+*/
+public abstract class Carta {
+
+    private boolean deFrente;
+
+    public Carta() {
+        // Constructor a resolver...
+    }
+
+    public void darVuelta() {
+        // Método a resolver...
+    }
+
+    public void mostrar() {
+        // Método a resolver...
+    }
+
+}
---
+++ src/resueltos/guia3/ej03/Mazo.java
@@ -0,0 +1,13 @@
+package resueltos.guia3.ej03;
+
+import java.util.ArrayList;
+
+/**
+* Código generado por la app UXFtoJava by Charly Cimino
+* @see https://github.com/CharlyCimino/uxf-to-java
+*/
+public class Mazo {
+
+    private ArrayList<Carta> cartas;
+
+}
