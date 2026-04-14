34e0b4cdbd78982175b16758a9202019603994c7
Charly <carlosecimino@gmail.com>
2026-04-14 16:31:54 -0300

    Renombrada la clase principal

--- src/guia01/ejercicio01/Test.java
+++ src/guia01/ejercicio01/Test.java
@@ -6,7 +6,7 @@ package guia01.ejercicio01;
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
-public class Principal {
+public class Test {
 
     public static void main(String[] args) {
         Persona per1 = new Persona("Maria", "Gomez", 1990);
